9def8de4247399c57948c602d0e6c50f1d56148b
Soni Gunawan <soni5424@gmail.com>
2026-06-24 09:29:25 +0700

    layout versi base

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Fina - Dashboard",
-  description: "Your personal finance dashboard",
+  description: "Your personal financial dashboard",
 };
 
 export default function DashboardPage() {
